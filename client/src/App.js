4a3083b7820664f2d1b4fd27237f33d7d228b252
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-08-19 03:13:42 -0400

    added flag to language toggle

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,25 @@ import Header from './components/shared/header/Header'
 import Home from './views/home/Home'
 
 export default function App() {
+    const [rus, setRus] = useState(false);
 
-  return (
-    <div className='App'>
-      <Container>
-      <Header />
-        <Switch>
-          <Route exact path="/" render={() =>
-            <Home
+    return (
+        <div className='App' >
+        <Container>
+                <Header
+                    rus={rus}
+                    handleToggle={() => setRus(!rus)}
+                />
             
-            />} />
-        </Switch>
-      </Container>
-    </div>
-  )
+                
+                <Switch >
+                <Route exact path="/"
+                    render={() =>
+                        <Home />
+                    }
+                /> 
+            </Switch > 
+        </Container> 
+        </div>
+    )
 }
--- client/src/components/shared/header/Header.jsx
+++ client/src/components/shared/header/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Radio } from 'semantic-ui-react'
+import { Radio, Icon, Flag } from 'semantic-ui-react'
+import Logo from '../../../assets/images/rcs_logo.png'
+import LanguageToggle from '../languageToggle/LanguageToggle'
 
-export default function Header() {
+export default function Header(props) {
+
+    const { rus, handleToggle } = props
     return (
-        <div>
+        <div style={{height: '15vh'}}>
+            <img src={Logo} style={{ height: '100%' }} alt="" />
+            <Icon name='mail outline' />
+            <Icon name='facebook' />
+            <Icon name='instagram' />
+            <Flag name='ru' />
             Header
-           <Radio toggle />
+           
+            <LanguageToggle rus={rus} handleToggle={handleToggle}/>
+
         </div>
     )
 }
